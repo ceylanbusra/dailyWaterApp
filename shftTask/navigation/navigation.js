8e02c4ae4b563638d62dc9223a805236b6f6753a
Büşra Ceylan <68326893+ceylanbusra@users.noreply.github.com>
2023-05-26 15:28:45 +0300

    update

--- shftTask/navigation/navigation.js
+++ shftTask/navigation/navigation.js
@@ -3,12 +3,17 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import SplashScreen from '../screens/splashScreen';
 import HomeScreen from '../screens/homeScreen';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import DetailScreen from '../screens/detailScreen';
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 const RootRouter = props => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Splash">
+      <Stack.Navigator
+        initialRouteName="Splash"
+        screenOptions={{headerShown: false}}>
         <Stack.Screen
           name="SplashScreen"
           component={SplashScreen}
@@ -23,11 +28,20 @@ const RootRouter = props => {
             headerTitleAlign: 'center',
           }}
           name="Home"
-          component={HomeScreen}
+          component={MyTabs}
         />
         {/* <Stack.Screen name="Detail" component={Detail} /> */}
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
+function MyTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Details" component={DetailScreen} />
+    </Tab.Navigator>
+  );
+}
+
 export default RootRouter;
